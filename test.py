0d6cee8a0ef9e3419777b136187f9a78573bfe98
PauMarquez99 <pauilimq99@gmail.com>
2023-05-17 11:40:55 -0600

    DJITello library and test code

---
+++ test.py
@@ -0,0 +1,28 @@
+from djitellopy import Tello
+import cv2, math, time
+
+tello = Tello()
+
+tello.connect()
+
+tello.streamon()
+frame_read = tello.get_frame_read()
+
+while True:
+    img = tello.get_frame_read().frame
+    cv2.imshow('stream', img)
+    cv2.waitKey(1)
+
+
+#for x in range (200):
+#    dist = tello.get_distance_tof()
+#    print(f"Distance {dist} cm. \n")
+
+#tello.end()
+#tello.takeoff()
+
+#tello.move_left(100)
+#tello.rotate_clockwise(90)
+#tello.move_forward(100)
+
+#tello.land()
