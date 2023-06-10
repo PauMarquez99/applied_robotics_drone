2de6c60529c50dd981db6c0d215cdc56e7fe2aeb
PauMarquez99 <pauilimq99@gmail.com>
2023-06-10 11:21:52 -0600

    closeness function

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import cv2, math, time
 def findFace(img):
     faceCascade = cv2.CascadeClassifier("codes/face_tracking/haarcascades/haarcascade_frontalface_default.xml")
     imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = faceCascade.detectMultiScale(imgGray, 1.2, 7)
+    faces = faceCascade.detectMultiScale(imgGray, 1.2, 8)
 
     faces_center_list = []
     faces_area_list = []
@@ -22,29 +22,33 @@ def findFace(img):
     else:
         return img, [[0,0], 0]
 
+def closeness(distance):
+    if distance > 70000:
+        move = "forward"
+        #tello.move_forward(10)
+    elif distance < 58500:
+        move = "backwards"
+        #tello.move_back(10)
+    else:
+        move = "nada"
+    return move
+
 tello = Tello()
-
 tello.connect()
-
 tello.streamon()
 frame_read = tello.get_frame_read()
 
 while True:
+    key = cv2.waitKey(1)
+    if key == 27:  # ESC
+        break
     img = tello.get_frame_read().frame
     proc_img, face_info = findFace(img)
-    print("Center: {} - Area: {}".format(face_info[0], face_info[1]))
+    move = closeness(face_info[1])
+    print("Center: {} - Area: {} - Move: {}".format(face_info[0], face_info[1], move))
     cv2.imshow('stream', proc_img)
-    cv2.waitKey(1)
 
-#for x in range (200):
-#    dist = tello.get_distance_tof()
-#    print(f"Distance {dist} cm. \n")p
 
 tello.end()
-#tello.takeoff()
-
-#tello.move_left(100)
-#tello.rotate_clockwise(90)
-#tello.move_forward(100)
-
+cv2.destroyAllWindows()
 #tello.land()
